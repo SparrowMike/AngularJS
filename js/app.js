2d8731858596de32dd4db9e7bd8d5c6da5968a39
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-01-22 18:52:28 +0800

    modules, controllers and scope

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
-var myApp = angular.module('myApp', [
-  'ngRoute',
-  'myControllers'
-]);
+var myApp = angular.module('myApp', []);
 
-myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'js/partials/search.html',
-      controller: 'SearchController'
-    })
-    .when('/details/:itemId', {
-      templateUrl: 'js/partials/details.html',
-      controller: 'DetailsController'
-    });
-}]);
-
+myApp.controller("MyController", function MyController($scope){
+  $scope.artist = {
+    "name":"Barot Bellingham",
+    "shortname":"Barot_Bellingham",
+    "reknown":"Royal Academy of Painting and Sculpture",
+    "bio":"Barot has just finished his final year at The Royal Academy of Painting and Sculpture, where he excelled in glass etching paintings and portraiture. Hailed as one of the most diverse artists of his generation, Barot is equally as skilled with watercolors as he is with oils, and is just as well-balanced in different subject areas. Barot's collection entitled \"The Un-Collection\" will adorn the walls of Gilbert Hall, depicting his range of skills and sensibilities - all of them, uniquely Barot, yet undeniably different"
+  }
+})
