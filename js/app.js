6a8997772e440eee362c6bc8259897d783d6ef8b
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-01-22 21:23:59 +0800

    binding inputs and controllers

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller("MyController", function MyController($scope, $http){
   $http.get('js/data.json').then(function(response){
-    $scope.artistis = response.data
+    $scope.artistis = response.data;
+    $scope.artistOrder = 'name';
   })
 })
